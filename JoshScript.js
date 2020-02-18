c5a782eaa3c7ad2748fec5df0e5b543de762b834
Josh Mountain <a01158690@se14l0124-12.ad.bcit.ca>
2020-02-18 12:24:43 -0800

    Big Chango Arrango

---
+++ JoshScript.js
@@ -0,0 +1,84 @@
+var data = {
+    qnum:0,
+    questions:[
+        {
+        title:"How is life?",
+        answer:"Good",
+        bgcolour:"yellow",
+        img:"http://placekitten.com/200/300"
+        },
+    
+    {
+        title:"What colour is a firetruck?",
+        answer:"Red",
+        bgcolour:"tomato",
+        img:"http://placekitten.com/100/300"
+        },
+    
+    {
+        title:"What colour is the ocean?",
+        answer:"Blue",
+        bgcolour:"Blue",
+        img:"http://placekitten.com/400/300"
+        }
+    ]
+
+}
+
+var qnum = 0;
+var questions = [
+   
+];
+
+var answers = [
+
+
+];
+
+var bgcolours = [
+  
+];
+
+var imgs = [
+
+];
+
+function Start(){
+   // document.querySelector(".Question h1").innerText = questions[qnum];
+   document.querySelector(".Question h1").innerText = data.questions[data.qnum].title;
+   document.body.style.backgroundColor = data.questions[data.qnum].bgcolour;
+   document.querySelector("#josh_r").innerHTML = "<img src='"+data.questions[data.qnum].img+"' />";
+}
+
+function CheckAnswer(){
+    var myInp = document.querySelector("#answer").value;
+    console.log(myInp);
+    if(myInp === data.questions[data.qnum].answer){
+        document.querySelector("#josh_r").innerHTML = "<h1>Correct! :)</h1>";
+        NextQ()
+      /*  if(qnum ===1){
+        document.querySelector(".Question h1").innerText = "Is 5 bigger than 4?";
+        answer = "Yes";
+        qnum = 2;
+        } else if (qnum === 2) {
+            document.querySelector(".Question h1").innerText = "What is the colour of a firetruck";
+            answer = "Red";
+            qnum = 3;
+
+        }
+      */  
+    } else {
+        document.querySelector("#josh_r").innerHTML = "<h1>WRONG! >:(</h1>"
+    }
+}
+
+function NextQ(){
+data.qnum++;
+document.querySelector(".Question h1").innerText = data.questions[data.qnum].title;
+document.body.style.backgroundColor = data.questions[data.qnum].bgcolour;
+document.querySelector("#josh_r").innerHTML = "<img src='"+data.questions[data.qnum].img+"' />";
+
+}
+
+
+Start()
